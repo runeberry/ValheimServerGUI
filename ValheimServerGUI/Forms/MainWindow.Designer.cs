2c66097fc0f751c722d6339f233905da3b3f6319
dolphinspired <ultima749@gmail.com>
2021-03-01 16:46:19 -0500

    Fix log window anchor

--- ValheimServerGUI/Forms/MainWindow.Designer.cs
+++ ValheimServerGUI/Forms/MainWindow.Designer.cs
@@ -300,7 +300,8 @@ namespace ValheimServerGUI.Forms
             // 
             // TextBoxLogs
             // 
-            this.TextBoxLogs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.TextBoxLogs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TextBoxLogs.BackColor = System.Drawing.SystemColors.Window;
             this.TextBoxLogs.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);

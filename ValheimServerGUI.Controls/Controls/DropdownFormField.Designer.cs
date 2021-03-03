9d57182e04f1f1f693d8bf2275b74846f55ae6dd
dolphinspired <ultima749@gmail.com>
2021-03-02 21:47:36 -0500

    Add DropdownFormField control

---
+++ ValheimServerGUI.Controls/Controls/DropdownFormField.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ValheimServerGUI.Controls
+{
+    partial class DropdownFormField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ComboBox = new System.Windows.Forms.ComboBox();
+            this.Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ComboBox
+            // 
+            this.ComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBox.FormattingEnabled = true;
+            this.ComboBox.Location = new System.Drawing.Point(9, 18);
+            this.ComboBox.Name = "ComboBox";
+            this.ComboBox.Size = new System.Drawing.Size(132, 23);
+            this.ComboBox.TabIndex = 0;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(9, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 15);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Label";
+            // 
+            // DropdownFormField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.ComboBox);
+            this.Name = "DropdownFormField";
+            this.Size = new System.Drawing.Size(150, 41);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ComboBox;
+        private System.Windows.Forms.Label Label;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/DropdownFormField.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ValheimServerGUI.Controls
+{
+    public partial class DropdownFormField : UserControl, IFormField<string>
+    {
+        public string EmptyText { get; set; } = "";
+
+        public string LabelText
+        {
+            get => this.Label.Text;
+            set => this.Label.Text = value;
+        }
+
+        public string Value
+        {
+            get
+            {
+                var selectedItem = this.ComboBox.SelectedItem?.ToString();
+
+                if (selectedItem == EmptyText)
+                {
+                    return null;
+                }
+
+                return selectedItem;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.ComboBox.SelectedItem = value;
+                    return;
+                }
+
+                if (this.DataSource.Contains(value))
+                {
+                    // Only allow value dropdown items to be set
+                    this.ComboBox.SelectedItem = value;
+                }
+            }
+        }
+
+        public IEnumerable<string> DataSource
+        {
+            get
+            {
+                var dataSource = this.ComboBox.DataSource as IEnumerable<string>;
+
+                if (dataSource == null)
+                {
+                    dataSource = new List<string>();
+                    this.ComboBox.DataSource = dataSource;
+                }
+
+                if (dataSource.Count() == 1 && dataSource.Single() == EmptyText)
+                {
+                    dataSource = dataSource.Take(0);
+                }
+
+                return dataSource;
+            }
+            set
+            {
+                if (value == null || !value.Any())
+                {
+                    value = new List<string> { EmptyText };
+                }
+
+                this.ComboBox.DataSource = value;
+            }
+        }
+
+        public DropdownFormField()
+        {
+            InitializeComponent();
+        }
+    }
+}

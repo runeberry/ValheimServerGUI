41a5f457b6d2dd66bfcd8bb47362d9ee3db82dd5
dolphinspired <ultima749@gmail.com>
2021-03-02 20:52:58 -0500

    Add TextFormField control

---
+++ ValheimServerGUI.Controls/Controls/TextFormField.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ValheimServerGUI.Forms.Controls
+{
+    partial class TextFormField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox.Location = new System.Drawing.Point(9, 20);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(132, 23);
+            this.TextBox.TabIndex = 0;
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(9, 2);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 15);
+            this.Label.TabIndex = 1;
+            this.Label.Text = "Label";
+            // 
+            // TextFormField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Label);
+            this.Controls.Add(this.TextBox);
+            this.Name = "TextFormField";
+            this.Size = new System.Drawing.Size(150, 43);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Label Label;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/TextFormField.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+
+namespace ValheimServerGUI.Forms.Controls
+{
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    public partial class TextFormField : UserControl
+    {
+        private const char PasswordChar = '●';
+        private const char PasswordCharDisabled = '\0';
+
+        public string LabelText
+        {
+            get => this.Label.Text;
+            set => this.Label.Text = value;
+        }
+
+        public string Value
+        {
+            get => this.TextBox.Text;
+            set => this.TextBox.Text = value;
+        }
+
+        public bool HideValue
+        {
+            get => this.TextBox.PasswordChar != PasswordCharDisabled;
+            set => this.TextBox.PasswordChar = value ? PasswordChar : PasswordCharDisabled;
+        }
+
+        public TextFormField()
+        {
+            InitializeComponent();
+        }
+    }
+}

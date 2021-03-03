af21d8ec06723702405048ff16a2fb995e0217d6
dolphinspired <ultima749@gmail.com>
2021-03-02 22:00:58 -0500

    Add CheckboxFormField control

---
+++ ValheimServerGUI.Controls/Controls/CheckboxFormField.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace ValheimServerGUI.Controls
+{
+    partial class CheckboxFormField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // CheckBox
+            // 
+            this.CheckBox.AutoSize = true;
+            this.CheckBox.Location = new System.Drawing.Point(9, 0);
+            this.CheckBox.Name = "CheckBox";
+            this.CheckBox.Size = new System.Drawing.Size(54, 19);
+            this.CheckBox.TabIndex = 0;
+            this.CheckBox.Text = "Label";
+            this.CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CheckboxFormField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CheckBox);
+            this.Name = "CheckboxFormField";
+            this.Size = new System.Drawing.Size(150, 17);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox CheckBox;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/CheckboxFormField.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace ValheimServerGUI.Controls
+{
+    public partial class CheckboxFormField : UserControl, IFormField<bool>
+    {
+        public string LabelText
+        {
+            get => this.CheckBox.Text;
+            set => this.CheckBox.Text = value;
+        }
+
+        public bool Value
+        {
+            get => this.CheckBox.Checked;
+            set => this.CheckBox.Checked = value;
+        }
+
+        public CheckboxFormField()
+        {
+            InitializeComponent();
+        }
+    }
+}

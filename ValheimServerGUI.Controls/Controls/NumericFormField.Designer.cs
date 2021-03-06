7ebada97e1287ad8cdc2a83f8af6ab4748539045
dolphinspired <ultima749@gmail.com>
2021-03-05 22:27:33 -0500

    Add NumericFormField control

---
+++ ValheimServerGUI.Controls/Controls/NumericFormField.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ValheimServerGUI.Controls
+{
+    partial class NumericFormField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label = new System.Windows.Forms.Label();
+            this.NumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Location = new System.Drawing.Point(9, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(35, 15);
+            this.Label.TabIndex = 2;
+            this.Label.Text = "Label";
+            // 
+            // NumericUpDown
+            // 
+            this.NumericUpDown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NumericUpDown.Location = new System.Drawing.Point(9, 18);
+            this.NumericUpDown.Name = "NumericUpDown";
+            this.NumericUpDown.Size = new System.Drawing.Size(57, 23);
+            this.NumericUpDown.TabIndex = 3;
+            // 
+            // NumericFormField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.NumericUpDown);
+            this.Controls.Add(this.Label);
+            this.Name = "NumericFormField";
+            this.Size = new System.Drawing.Size(75, 41);
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.NumericUpDown NumericUpDown;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/NumericFormField.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+
+namespace ValheimServerGUI.Controls
+{
+    public partial class NumericFormField : UserControl, IFormField<int>
+    {
+        public string LabelText
+        {
+            get => this.Label.Text;
+            set => this.Label.Text = value;
+        }
+
+        public int Value
+        {
+            get => (int)this.NumericUpDown.Value;
+            set => this.NumericUpDown.Value = value;
+        }
+
+        public int Minimum
+        {
+            get => (int)this.NumericUpDown.Minimum;
+            set => this.NumericUpDown.Minimum = value;
+        }
+
+        public int Maximum
+        {
+            get => (int)this.NumericUpDown.Maximum;
+            set => this.NumericUpDown.Maximum = value;
+        }
+
+        public NumericFormField()
+        {
+            InitializeComponent();
+        }
+    }
+}

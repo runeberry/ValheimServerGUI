290e890e498a2171de8a622e3038f37d9eb8861f
dolphinspired <ultima749@gmail.com>
2021-03-11 20:56:10 -0500

    Add PlayerInfo and PlayerStatus

---
+++ ValheimServerGUI/Game/PlayerInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ValheimServerGUI.Game
+{
+    public class PlayerInfo
+    {
+        // I'm assuming you can't put a tab in your character's name...
+        private const char Separator = '\t';
+
+        public string PlayerName { get; set; }
+
+        public string SteamId { get; set; }
+
+        public DateTimeOffset LastOnline { get; set; }
+
+        public PlayerStatus PlayerStatus { get; set; }
+
+        public static PlayerInfo Parse(string info)
+        {
+            var parts = info.Split(Separator);
+
+            return new PlayerInfo
+            {
+                PlayerName = parts[1],
+                SteamId = parts[2],
+                LastOnline = DateTimeOffset.Parse(parts[3]),
+            };
+        }
+
+        public override string ToString()
+        {
+            // todo: Sanitize PlayerName input
+            return string.Join(Separator, PlayerName, SteamId, LastOnline);
+        }
+    }
+}
---
+++ ValheimServerGUI/Game/PlayerStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace ValheimServerGUI.Game
+{
+    public enum PlayerStatus
+    {
+        Offline = 0,
+        Joining = 1,
+        Online = 2,
+        Leaving = 3,
+    }
+}

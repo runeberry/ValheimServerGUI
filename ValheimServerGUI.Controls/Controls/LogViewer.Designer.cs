cb2214dcf870e59002607851d1f0221102f33c4b
dolphinspired <ultima749@gmail.com>
2021-03-23 23:23:49 -0400

    Add control: LogViewer

---
+++ ValheimServerGUI.Controls/Controls/LogViewer.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ValheimServerGUI.Controls
+{
+    partial class LogViewer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.TextBox.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TextBox.Location = new System.Drawing.Point(0, 0);
+            this.TextBox.Multiline = true;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.ReadOnly = true;
+            this.TextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox.Size = new System.Drawing.Size(300, 150);
+            this.TextBox.TabIndex = 0;
+            // 
+            // LogViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.TextBox);
+            this.Name = "LogViewer";
+            this.Size = new System.Drawing.Size(300, 150);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/LogViewer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ValheimServerGUI.Controls
+{
+    public partial class LogViewer : UserControl
+    {
+        public const string DefaultLogView = "DefaultLogView";
+
+        private readonly ConcurrentDictionary<string, List<string>> LogsByView = new();
+
+        private string _logView = DefaultLogView;
+        public string LogView
+        {
+            get => this._logView;
+            set
+            {
+                value ??= DefaultLogView;
+                if (value == this._logView) return;
+                this._logView = value;
+                this.LogViewChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public event EventHandler LogViewChanged;
+
+        public LogViewer()
+        {
+            InitializeComponent();
+
+            this.LogViewChanged += this.OnLogViewChanged;
+        }
+
+        public void AddLog(string message)
+        {
+            this.AddLogToView(message, DefaultLogView);
+        }
+
+        public void AddLogToView(string message, string viewName)
+        {
+            this.StoreLog(message, viewName);
+
+            if (viewName == this.LogView)
+            {
+                this.AppendLine(message);
+            }
+        }
+
+        public void ClearLogs()
+        {
+            this.LogsByView.Clear();
+            
+            this.ClearLines();
+        }
+
+        public string GetCurrentViewText()
+        {
+            return this.TextBox.Text;
+        }
+
+        #region Non-public methods
+
+        private void StoreLog(string message, string viewName)
+        {
+            if (!this.LogsByView.TryGetValue(viewName, out var list))
+            {
+                list = new List<string>();
+                if (!this.LogsByView.TryAdd(viewName, list))
+                {
+                    list = this.LogsByView[viewName];
+                }
+            }
+
+            list.Add(message);
+        }
+
+        private void AppendLine(string line)
+        {
+            if (line == null) return;
+
+            if (this.TextBox.Text == null || this.TextBox.Text.Length == 0)
+            {
+                this.TextBox.Text = line;
+            }
+            else
+            {
+                this.TextBox.AppendText(Environment.NewLine + line);
+            }
+        }
+
+        private void ClearLines()
+        {
+            this.TextBox.Text = string.Empty;
+        }
+
+        private void OnLogViewChanged(object sender, EventArgs args)
+        {
+            this.ClearLines();
+
+            if (this.LogsByView.TryGetValue(this.LogView, out var logs))
+            {
+                foreach (var log in logs)
+                {
+                    this.AppendLine(log);
+                }
+            }
+        }
+
+        #endregion
+    }
+}

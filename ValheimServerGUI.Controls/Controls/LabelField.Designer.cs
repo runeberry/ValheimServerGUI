1725d6220894b6a03eddaff761c60c2c3ddd00e2
dolphinspired <ultima749@gmail.com>
2021-03-27 22:42:37 -0400

    Add LabelField control

---
+++ ValheimServerGUI.Controls/Controls/LabelField.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace ValheimServerGUI.Controls
+{
+    partial class LabelField
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormLabel = new System.Windows.Forms.Label();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.HelpLabel = new ValheimServerGUI.Controls.HelpLabel();
+            this.SuspendLayout();
+            // 
+            // FormLabel
+            // 
+            this.FormLabel.Location = new System.Drawing.Point(9, 0);
+            this.FormLabel.Name = "FormLabel";
+            this.FormLabel.Size = new System.Drawing.Size(64, 15);
+            this.FormLabel.TabIndex = 0;
+            this.FormLabel.Text = "Label";
+            this.FormLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ValueLabel
+            // 
+            this.ValueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ValueLabel.Location = new System.Drawing.Point(73, 0);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(64, 15);
+            this.ValueLabel.TabIndex = 1;
+            // 
+            // HelpLabel
+            // 
+            this.HelpLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.HelpLabel.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.HelpLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.HelpLabel.Location = new System.Drawing.Point(138, 0);
+            this.HelpLabel.Name = "HelpLabel";
+            this.HelpLabel.Size = new System.Drawing.Size(12, 15);
+            this.HelpLabel.TabIndex = 2;
+            // 
+            // LabelField
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.HelpLabel);
+            this.Controls.Add(this.ValueLabel);
+            this.Controls.Add(this.FormLabel);
+            this.Name = "LabelField";
+            this.Size = new System.Drawing.Size(150, 15);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FormLabel;
+        private System.Windows.Forms.Label ValueLabel;
+        private HelpLabel HelpLabel;
+    }
+}
---
+++ ValheimServerGUI.Controls/Controls/LabelField.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ValheimServerGUI.Controls
+{
+    public partial class LabelField : UserControl, IFormField<string>
+    {
+        #region IFormField implementation
+
+        public string LabelText
+        {
+            get => this.FormLabel.Text;
+            set => this.FormLabel.Text = value;
+        }
+
+        [Editor("System.ComponentModel.Design.MultilineStringEditor", "System.Drawing.Design.UITypeEditor")]
+        public string HelpText
+        {
+            get => this.HelpLabel.Text;
+            set => this.HelpLabel.Text = value;
+        }
+
+        public string Value
+        {
+            get => this.ValueLabel.Text;
+            set
+            {
+                if (this.ValueLabel.Text == value) return;
+
+                this.ValueLabel.Text = value;
+                this.ValueChanged?.Invoke(this, value);
+            }
+        }
+
+        public event EventHandler<string> ValueChanged;
+
+        #endregion
+
+        private double _lsr = 0.5;
+        public double LabelSplitRatio
+        {
+            get => this._lsr;
+            set
+            {
+                this._lsr = value;
+                this.ResizeLabels();
+            }
+        }
+
+        public ContentAlignment LabelTextAlign
+        {
+            get => this.FormLabel.TextAlign;
+            set => this.FormLabel.TextAlign = value;
+        }
+
+        public ContentAlignment ValueTextAlign
+        {
+            get => this.ValueLabel.TextAlign;
+            set => this.ValueLabel.TextAlign = value;
+        }
+
+        public LabelField()
+        {
+            InitializeComponent();
+
+            this.ResizeLabels();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            this.ResizeLabels();
+        }
+
+        private void ResizeLabels()
+        {
+            var fillWidth = this.Width - this.FormLabel.Location.X - this.HelpLabel.Width;
+            var split = Math.Min(1.0, Math.Max(0.0, this.LabelSplitRatio));
+
+            // The Label and Value should each fill up 50% of the available space
+            this.FormLabel.Width = (int)(fillWidth * split);
+            this.ValueLabel.Width = (int)(fillWidth * (1-split));
+
+            // The Value should always be anchored directly to the right of the Label
+            var locX = this.FormLabel.Location.X + this.FormLabel.Width;
+            this.ValueLabel.Location = new Point(locX, this.ValueLabel.Location.Y);
+    }
+    }
+}

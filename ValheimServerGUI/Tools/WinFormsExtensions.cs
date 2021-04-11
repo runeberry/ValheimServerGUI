48a7d9c573060370cd45a34ce77411586e78f515
dolphinspired <ultima749@gmail.com>
2021-04-11 19:39:07 -0400

    Add extensions for async event handlers

--- ValheimServerGUI/Tools/WinFormsExtensions.cs
+++ ValheimServerGUI/Tools/WinFormsExtensions.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
 using System.Resources;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ValheimServerGUI.Tools
@@ -40,6 +40,36 @@ namespace ValheimServerGUI.Tools
                 }
 
                 action();
+            };
+        }
+
+        public static EventHandler<TArgs> BuildEventHandlerAsync<TArgs>(this Control control, Func<TArgs, Task> taskFunc, int taskDelay = 0)
+        {
+            return async (sender, args) =>
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Func<TArgs, Task>(taskFunc), new object[] { args });
+                    return;
+                }
+
+                if (taskDelay > 0) await Task.Delay(taskDelay);
+                await Task.Run(() => taskFunc(args));
+            };
+        }
+
+        public static EventHandler BuildEventHandlerAsync(this Control control, Func<Task> taskFunc, int taskDelay = 0)
+        {
+            return async (sender, args) =>
+            {
+                if (control.InvokeRequired)
+                {
+                    control.Invoke(new Func<Task>(taskFunc));
+                    return;
+                }
+
+                if (taskDelay > 0) await Task.Delay(taskDelay);
+                await Task.Run(() => taskFunc());
             };
         }
 

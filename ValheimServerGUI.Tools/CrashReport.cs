83be026ba91b21d0f12fa45d619049fc0572a8cb
dolphinspired <ultima749@gmail.com>
2021-05-01 22:13:14 -0400

    Add CrashReport model

---
+++ ValheimServerGUI.Tools/CrashReport.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ValheimServerGUI.Tools
+{
+    public class CrashReport
+    {
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTimeOffset Timestamp { get; set; }
+
+        [JsonProperty("appVersion")]
+        public string AppVersion { get; set; }
+
+        [JsonProperty("osVersion")]
+        public string OsVersion { get; set; }
+
+        [JsonProperty("dotnetVersion")]
+        public string DotnetVersion { get; set; }
+
+        [JsonProperty("additionalInfo")]
+        public Dictionary<string, string> AdditionalInfo { get; set; }
+
+        [JsonProperty("logs")]
+        public List<string> Logs { get; set; }
+    }
+}

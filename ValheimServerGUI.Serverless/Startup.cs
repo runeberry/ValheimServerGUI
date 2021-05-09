9f77385c1a23b6c25d22c57422f6dccb310cd1ee
dolphinspired <ultima749@gmail.com>
2021-05-09 15:37:33 -0400

    Add API key auth middleware to serverless app

--- ValheimServerGUI.Serverless/Startup.cs
+++ ValheimServerGUI.Serverless/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using ValheimServerGUI.Serverless.Middleware;
 
 namespace ValheimServerGUI.Serverless
 {
@@ -42,6 +36,8 @@ namespace ValheimServerGUI.Serverless
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.Use(RuneberryAuthMiddleware.Authorize);
 
             app.UseEndpoints(endpoints =>
             {

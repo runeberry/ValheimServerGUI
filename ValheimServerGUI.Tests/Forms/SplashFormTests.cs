12a554be74258cd0abe87707ec15e6b3516df547
dolphinspired <ultima749@gmail.com>
2021-05-14 15:42:12 -0400

    Stub out SplashFormTests

---
+++ ValheimServerGUI.Tests/Forms/SplashFormTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ValheimServerGUI.Forms;
+using Xunit;
+
+namespace ValheimServerGUI.Tests.Forms
+{
+    public class SplashFormTests : BaseTest
+    {
+        public SplashFormTests()
+        {
+        }
+
+        [Fact]
+        public void CanInitSplashForm()
+        {
+            this.GetForm<SplashForm>();
+        }
+    }
+}

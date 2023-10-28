85d475dcb6da17d2603fbdee6f687ae9a242edcf
dolphinspired <brogan@runeberry.com>
2023-10-28 06:34:21 -0400

    Create constants from modifier values

--- ValheimServerGUI/Game/WorldGenModifiers.cs
+++ ValheimServerGUI/Game/WorldGenModifiers.cs
@@ -10,6 +10,36 @@ namespace ValheimServerGUI.Game
         public const string Raids = "raids";
         public const string Portals = "portals";
 
+        public static class Values
+        {
+            public const string CombatVeryEasy = "veryeasy";
+            public const string CombatEasy = "easy";
+            public const string CombatHard = "hard";
+            public const string CombatVeryHard = "veryhard";
+
+            public const string DeathPenaltyCasual = "casual";
+            public const string DeathPenaltyVeryEasy = "veryeasy";
+            public const string DeathPenaltyEasy = "easy";
+            public const string DeathPenaltyHard = "hard";
+            public const string DeathPenaltyHardcore = "hardcore";
+
+            public const string ResourcesMuchLess = "muchless";
+            public const string ResourcesLess = "less";
+            public const string ResourcesMore = "more";
+            public const string ResourcesMuchMore = "muchmore";
+            public const string ResourcesMost = "most";
+
+            public const string RaidsNone = "none";
+            public const string RaidsMuchLess = "muchless";
+            public const string RaidsLess = "less";
+            public const string RaidsMore = "more";
+            public const string RaidsMuchMore = "muchmore";
+
+            public const string PortalsCasual = "casual";
+            public const string PortalsHard = "hard";
+            public const string PortalsVeryHard = "veryhard";
+        }
+
         public static readonly IReadOnlyList<string> All = new List<string>()
         {
             Combat,
@@ -25,52 +55,52 @@ namespace ValheimServerGUI.Game
                 Combat,
                 new[]
                 {
-                    "veryeasy",
-                    "easy",
-                    "hard",
-                    "veryhard",
+                    Values.CombatVeryEasy,
+                    Values.CombatEasy,
+                    Values.CombatHard,
+                    Values.CombatVeryHard,
                 }
             },
             {
                 DeathPenalty,
                 new[]
                 {
-                    "casual",
-                    "veryeasy",
-                    "easy",
-                    "hard",
-                    "hardcore",
+                    Values.DeathPenaltyCasual,
+                    Values.DeathPenaltyVeryEasy,
+                    Values.DeathPenaltyEasy,
+                    Values.DeathPenaltyHard,
+                    Values.DeathPenaltyHardcore,
                 }
             },
             {
                 Resources,
                 new[]
                 {
-                    "muchless",
-                    "less",
-                    "more",
-                    "muchmore",
-                    "most",
+                    Values.ResourcesMuchLess,
+                    Values.ResourcesLess,
+                    Values.ResourcesMore,
+                    Values.ResourcesMuchMore,
+                    Values.ResourcesMost,
                 }
             },
             {
                 Raids,
                 new[]
                 {
-                    "none",
-                    "muchless",
-                    "less",
-                    "more",
-                    "muchmore",
+                    Values.RaidsNone,
+                    Values.RaidsMuchLess,
+                    Values.RaidsLess,
+                    Values.RaidsMore,
+                    Values.RaidsMuchMore,
                 }
             },
             {
                 Portals,
                 new[]
                 {
-                    "casual",
-                    "hard",
-                    "veryhard",
+                    Values.PortalsCasual,
+                    Values.PortalsHard,
+                    Values.PortalsVeryHard,
                 }
             },
         };
